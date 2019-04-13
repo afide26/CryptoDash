fa35ce06390fe8968dcaeb3e2dd0d03d9937883f
Alan Fidelino <afidedev26@gmail.com>
2019-04-13 15:57:46 +1000

    Create CoinTile, CoinImage, CoinGridHeaderGrid

--- src/Settings/CoinGrid.jsx
+++ src/Settings/CoinGrid.jsx
@@ -1,22 +1,26 @@
 import React from "react";
 import styled from "styled-components";
 import { AppContext } from "../App/AppProvider";
-import { SelectableTile } from "../Shared/Tile";
+import CoinTile from "./CoinTile";
 
 const CoinGridStyled = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   grid-gap: 15px;
+  margin-top: 40px;
 `;
 
+function getCoinsToDisplay(coinList) {
+  return Object.keys(coinList).slice(0, 100);
+}
 export default function() {
   return (
     <AppContext.Consumer>
       {({ coinList }) => (
         <CoinGridStyled>
-          {Object.keys(coinList).map((coinKey, index) => (
-            <SelectableTile key={index}>{coinKey}</SelectableTile>
-          ))}
+          {getCoinsToDisplay(coinList).map((coinKey, index) => {
+            return <CoinTile key={index} coinKey={coinKey} />;
+          })}
         </CoinGridStyled>
       )}
     </AppContext.Consumer>
--- src/Settings/index.jsx
+++ src/Settings/index.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import WelcomeMessage from "./WelcomeMessage";
 import ConfirmButton from "./ConfirmButton";
 import Page from "../Shared/Page";
-import Grid from "./Grid";
+import CoinGrid from "./CoinGrid";
 
 export default function() {
   return (
     <Page name="settings">
       <WelcomeMessage />
       <ConfirmButton />
-      <Grid />
+      <CoinGrid />
     </Page>
   );
 }
