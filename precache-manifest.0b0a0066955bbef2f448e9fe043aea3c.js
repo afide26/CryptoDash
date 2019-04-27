48f8c8ee9d5ced682472d1186a3932afb2bd46db
Alan Fidelino <afidedev26@gmail.com>
2019-04-27 18:52:47 +1000

    Updates

---
+++ precache-manifest.0b0a0066955bbef2f448e9fe043aea3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5591c0be167dfc1d92f3",
+    "url": "/CryptoDash/static/js/runtime~main.dbe0dabc.js"
+  },
+  {
+    "revision": "a140c00c4bae9907bf36",
+    "url": "/CryptoDash/static/js/main.5c1ab100.chunk.js"
+  },
+  {
+    "revision": "19924aca360043391550",
+    "url": "/CryptoDash/static/js/2.52ba53ba.chunk.js"
+  },
+  {
+    "revision": "a140c00c4bae9907bf36",
+    "url": "/CryptoDash/static/css/main.c0ada1c6.chunk.css"
+  },
+  {
+    "revision": "25b03faa7bf5f37b04c12b0c3b0fb04f",
+    "url": "/CryptoDash/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/slack-redux/precache-manifest.f3ff6c83e45604290669039c332b0c9f.js"
+  "/CryptoDash/precache-manifest.0b0a0066955bbef2f448e9fe043aea3c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/slack-redux/index.html", {
+workbox.routing.registerNavigationRoute("/CryptoDash/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
